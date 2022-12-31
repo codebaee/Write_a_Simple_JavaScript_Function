4bc2ecc0eb876569955106f8e7198c4aff330b8c
codebaee <codingbaee@gmail.com>
2022-12-30 19:18:00 -0500

    Write a simple JavaScript Function Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+//Write a javascript code that prints out your name, height and country on the screen.
+function myInfo(firstName, height, country) {
+   alert (
+    "Hey there! My name is " + 
+        firstName + 
+        ", my height is " + 
+        height +
+        " and I'm from the " + 
+        country
+   );
+}
+
+myInfo("Ashley" , ("5'5") , "USA. Nice to meet you!") 
